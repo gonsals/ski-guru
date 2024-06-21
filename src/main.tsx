6577003ab9bc812f91128cff707a228a683b1af7
marcgonzaleztarrio <marcgonzaleztarrio@gmail.com>
2024-06-22 01:33:55 +0200

    prueba

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom";
 import "./styles/main.css";
+import AppRouter from "./router";
+import { initializeApp } from "firebase/app";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const firebaseConfig = {
+    apiKey: "AIzaSyCoP938TtXIZngJlbVWFPe45OL-_SF7quk",
+    authDomain: "skiguru-ce4a4.firebaseapp.com",
+    projectId: "skiguru-ce4a4",
+    storageBucket: "skiguru-ce4a4.appspot.com",
+    messagingSenderId: "644638437564",
+    appId: "1:644638437564:web:d25e7512fbf7d18b4260b9",
+    measurementId: "G-JZ53L4875N",
+};
+
+initializeApp(firebaseConfig);
+
+ReactDOM.render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>
+        <AppRouter />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
